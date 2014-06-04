9ecad90deaa508454dd031935ab9d8b843dfb77b
nickvleeuwen.nvl@gmail.com <nickvleeuwen.nvl@gmail.com@9b15c8b8-22b1-5825-f056-898cc4f60771>
2014-06-04 14:59:14 +0000

    Final versie 2.0

---
+++ trunk/bierplicatie2.0newfolder/Bierplicatie2.0/Bierplicatie2.0/code/Muziek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bierplicatie2._0.code
+{
+    class Muziek
+    {
+        private SoundPlayer muziek = new SoundPlayer();
+
+        public Muziek()
+        {
+            // TODO: Complete member initialization
+        }
+
+        public void afspelen(string locatie)
+        {
+            if (File.Exists(locatie))
+            {
+                muziek.SoundLocation = locatie;
+                muziek.Play();
+            }
+            else
+            {
+                muziek.SoundLocation = @"C:\bierplicatie\Media\Emma.wav";
+                muziek.Play();
+            }
+        }
+
+    }
+}
